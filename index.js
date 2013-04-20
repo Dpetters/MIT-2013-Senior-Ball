5501cb15345d226b260ab09da7147b4cd04377f3
Dmitrij Petters <Dpetters91@gmail.com>
2013-04-19 23:45:53 -0400

    site updates

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
-    $(window).resize(function(){
-	$('#horizontal_bar').css({
-	    top: ($(window).height() - $('#horizontal_bar').outerHeight())/2
-	});
-    });
+  $(window).resize(function(){
+	  $('#horizontal_bar').css({top: ($(window).height() - $('#horizontal_bar').outerHeight())/2});
+  });
 
-    $("#navigation a").live('click', function(e){
-        $(".page_contents").fadeOut('slow');
-        e.preventDefault();
-    });
-    $("#general_info_link").live('click', function(e){
-        $("#general_info").delay(300).fadeIn('slow');
-        e.preventDefault();
-    });
+  navigating = false
+  $("#navigation a").live('click', function(e){
+    if (!navigating) {
+      navigating = true
+      $(".page_contents").fadeOut('slow');
+      $("#" + $(this).attr("id").split("_").slice(0, -1).join("_")).delay(300).fadeIn('slow');
+      navigating = false
+    }
+    e.preventDefault();
+  });
+  
+  // To initially run the function:
+  $(window).resize();
 
-    // To initially run the function:
-    $(window).resize();
-
-    $("#horizontal_bar").delay(500).animate({width:'100%'}, 1000);
-    $("#horizontal_bar_content").delay(1000).animate({width:'100%'}, 1000);
-    $("#vertical_bar").delay(2000).animate({height:'100%'}, 'slow');
-    $("#vertical_content").delay("2800").fadeIn('slow');
-    $("#general_info").delay(3000).fadeIn('slow');
+  $("#horizontal_bar").delay(500).animate({width:'100%'}, 1000);
+  $("#horizontal_bar_content").delay(1000).animate({width:'100%'}, 1000);
+  $("#vertical_bar").delay(2000).animate({height:'100%'}, 'slow');
+  $("#vertical_content").delay("2800").fadeIn('slow');
+  $("#general_info").delay(3000).fadeIn('slow');
 });
 
